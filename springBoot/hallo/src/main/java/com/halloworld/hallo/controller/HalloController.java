8e5aa60355da9ea18997e5cab1c3a10d5a56050a
Pricilla Souza <pricilla_souzacs@hotmail.com>
2020-06-23 09:54:35 -0300

    Subindo spring atividade incial

---
+++ springBoot/hallo/src/main/java/com/halloworld/hallo/controller/HalloController.java
@@ -0,0 +1,19 @@
+package com.halloworld.hallo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//DEFININDO PONTO DE ACESSO PARA ESTA APLICAÇÃO
+@RestController
+@RequestMapping ("/hallo")
+
+public class HalloController {
+	
+	@GetMapping
+	public String hello() {
+		return "Hallo Generation!";
+	}
+ 
+
+}
